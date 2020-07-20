fc512bd003c67f1ccc2355702221f934bb5eeaee
Denys <gryshko_denys@gmail.com>
2020-07-20 16:10:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8758f7d299f4d9363f87977cceac2196.js"
+  "./precache-manifest.6440d7ef60844aeaeb1dc1a0afe8d0ff.js"
 );
 
 self.addEventListener('message', (event) => {
